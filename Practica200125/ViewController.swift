fca35041ea35d2668a7d38d4a54a34106c14916f
ramon baeza yepez <By_ramon@outlook.com>
2020-01-25 12:53:45 -0600

    constantes puestos

--- Practica200125/ViewController.swift
+++ Practica200125/ViewController.swift
@@ -9,12 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var txtCounter: UILabel!
+    var counter = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        counter = 0
     }
 
-
+    @IBAction func clickCount(_ sender: UIButton) {
+        self.counter += 1
+        txtCounter.text = "Clicks \(self.counter)"
+    }
+    
 }
 
